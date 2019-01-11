7139ead9275d8c8e6bb523733776f12dae8c8666
Jason Florez <jaanflor@bancolombia.com.co>
2019-01-10 21:20:43 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,17 @@
 // Puerto
 // ===========
 process.env.PORT = process.env.PORT || 3000;
+// ===========
+// Entorno
+// ===========
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// ===========
+// Base de datos
+// ===========
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:P4ssw0rd@ds155263.mlab.com:55263/cafe';
+};
+process.env.URLDB = urlDB;
