91bcc834858a83856b86be647f16ce97ba44b48a
Jason Florez <jaanflor@bancolombia.com.co>
2019-01-10 21:40:38 -0500

    Variable entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:P4ssw0rd@ds155263.mlab.com:55263/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 process.env.URLDB = urlDB;
